7ebf7a006b5f2b6bde09fd09c1603457695c562b
dronebeelinux <linuxbee@pm.me>
2023-11-20 01:24:02 +0000

    updating to send output to new file

--- replace_env.sh
+++ replace_env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Function to replace placeholders in the zone file using sed
+# Function to replace placeholders in the zone file using sed and create a new file
 replace_placeholders() {
     zone_file="$1"
     env_file="$2"
+    output_file="$3"
 
     if [ ! -f "$zone_file" ]; then
         echo "File $zone_file not found"
@@ -17,16 +18,19 @@ replace_placeholders() {
     fi
     source "$env_file"
 
+    cp "$zone_file" "$output_file"  # Create a copy of the original file
+
     for placeholder in $(cut -d= -f1 "$env_file"); do
         value="${!placeholder}"
-        sed -i "s/${placeholder//\./\\.}/${value//\./\\.}/g" "$zone_file"
+        sed -i "s/${placeholder//\./\\.}/${value//\./\\.}/g" "$output_file"
     done
 
-    echo "Placeholder values replaced successfully in $zone_file"
+    echo "Placeholder values replaced successfully. Updated file: $output_file"
 }
 
 # Example usage:
 zone_file="path/to/your/zone/file.db"
 env_file="placeholder.env"
+output_file="path/to/output/zone/file.db.updated"  # Provide the path for the updated file
 
-replace_placeholders "$zone_file" "$env_file"
+replace_placeholders "$zone_file" "$env_file" "$output_file"
